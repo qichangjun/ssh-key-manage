ecd2558d7a87e5ffe65d54f45c2a7e3de65f5e51
qichangjun <1061427304@qq.com>
2018-01-16 16:41:51 +0800

    添加第5章练习

---
+++ chapter 5/practise.cpp
@@ -0,0 +1,57 @@
+//  Charpter 5
+//  practise.cpp
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+using namespace std;
+
+int main()
+{
+    /** 练习题2
+     *  int i;
+     *  for (i = 0;i < 5; i++)
+     *      cout << i;
+     *      cout << endl;
+     *  打印内容 : 0,1,2,3,4
+    **/
+
+    /** 练习题3
+     *  int j;
+     *  for (j = 0; j < 11; j +=3)
+     *      cout << j;
+     *  cout << endl << j << endl;
+     *  打印内容 : 0,3,6,9
+     *              12
+    **/
+
+    /** 练习题4
+     * int j = 5;
+     * while ( ++j < 9 )
+     *      cout << j++ << endl;
+     * 打印内容 : 6,8
+     * 
+     **/ 
+
+    /** 练习题6
+     *  int i;
+     *  for (i = 1; i <= 64; i = i*2)
+     *      cout << i << endl;
+    **/ 
+
+    /** 练习题8
+     *  下面的语句是否有效，原因？他完成的是什么工作?
+     *  int x = (1,024);
+     *  八进制为20,对x赋值20,
+     *  int y;
+     *  y = 1,024;
+     *  有效，将y设置成1
+    **/ 
+
+    
+    return 0;
+}
+
+
